6408cc597d0ecfdeca8dff7ebb14dfaab9afae55
Patrick Mueller <patrick.mueller@elastic.co>
2024-06-26 07:53:16 -0400

    install from template, then clean up

---
+++ observablehq.config.js
@@ -0,0 +1,8 @@
+// See https://observablehq.com/framework/config for documentation.
+export default {
+  title:     'eslp Dashboards',
+  root:      'src',
+  output:    'docs',
+  cleanUrls: false,
+  search:    true,
+}
